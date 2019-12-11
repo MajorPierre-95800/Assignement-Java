d90062477f711829ad89e23c87f338011e8b8265
MajorPierre-95800 <58752620+MajorPierre-95800@users.noreply.github.com>
2019-12-11 12:32:18 +0000

    Add files via upload

---
+++ Assignement2/src/Assignement2/DateTimeService.java
@@ -0,0 +1,21 @@
+package Assignement2;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateTimeService
+{
+   private Calendar calendar;
+
+   public DateTimeService()
+   {
+         this.calendar = Calendar.getInstance();
+   }
+
+
+   public String getDateAndTime()
+   {
+         Date d = this.calendar.getTime();
+         return "The Raspberry time is: " + d.toString();        
+   }        
+}
---
+++ Assignement2/src/Assignement2/Temperature.java
@@ -0,0 +1,29 @@
+package Assignement2;
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Temperature {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader Lecture = null;
+		String ligne;
+		try 
+		{
+			Lecture = new BufferedReader(new FileReader("/sys/class/thermal/thermal_zone0/temp"));
+		}
+		catch (FileNotFoundException exc) 
+		{
+			System.out.println("Erreur d'ouverture");
+		}
+		while((ligne = Lecture.readLine()) != null) 
+		{
+			System.out.println(ligne);
+			Lecture.close();
+		}
+
+	}
+
+}
